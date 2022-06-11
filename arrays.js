7ca9689f707b050b32e0c34d7c15e3b232b07516
PavelSL <spav112@yandex.ru>
2022-06-11 21:48:51 +0300

    shops

--- arrays.js
+++ arrays.js
@@ -65,3 +65,46 @@ function availableCurr(arr, missingCurr) {
 }
 
 console.log(availableCurr(additionalCurrencies));
+
+// shops
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let {shops: shopsOptions} = data;
+    let volume = 0;
+    shopsOptions.forEach((item) => {
+        let shopVolume = item.width*item.length*data.height;
+        volume += shopVolume;
+    });
+    let cost = volume * data.moneyPer1m3;
+    console.log(cost);
+    if (cost <= shoppingMallData.budget) {
+        return `Бюджета достаточно`;
+    } else {
+        return `Бюджета недостаточно`;
+    }
+}
+
+console.log(isBudgetEnough(shoppingMallData));
