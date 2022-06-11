b4be9f80f7c8303a8743a55f795a6395c80e3ec9
PavelSL <spav112@yandex.ru>
2022-06-11 16:41:20 +0300

    objects

--- functions.js
+++ functions.js
@@ -139,3 +139,46 @@ function fib(num) {
 }
 
 console.log(fib(1));
+
+// experience
+// 1
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng', 'ua'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%',
+            'C++': '15%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs(plan) {
+        let {age} = plan,
+            {languages} = plan.skills;
+        let lang = languages.join(' ').toUpperCase();
+        let text = `Мне ${age} и я владею языками: ${lang}`;
+        return text;
+    }
+};
+
+function showExperience(plan) {
+    let {exp} = plan.skills;
+    return exp;
+}
+
+console.log(showExperience(personalPlanPeter));
+
+function showProgrammingLangs(plan) {
+    let {programmingLangs} = plan.skills;
+    let langProgress = '';
+    for (let key in programmingLangs) {
+        langProgress += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+    }
+    return langProgress;
+}
+
+console.log(showProgrammingLangs(personalPlanPeter));
+
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
